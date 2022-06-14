36cb578187335b0fa59f2da6934175f38f412d83
Miike0 <Miguel.A.INCO@gmail.com>
2022-06-14 01:21:30 -0500

    Se agregaron nuevas monedas al conversor y se creó un menú

--- conversorMonedas/conversor.py
+++ conversorMonedas/conversor.py
@@ -1,22 +1,45 @@
-from cmath import e
+menu = """
+Bienvenido al conversor de monedas
 
+1.-mxn-usd
+2.-usd-mxn
+3.-col-usd
+4.-ars-usd
 
-tipo_cambio = int(input("¿Quieres convertir mx-usd(1) o usd-mx?(2)"))
+Elige una opción:
+"""
 
-if tipo_cambio >= 1 and tipo_cambio <= 2:
-    if tipo_cambio == 1:
+opcion = int(input(menu))
+
+if opcion >= 1 and opcion <= 4:
+    if opcion == 1:
         pesos_mexicanos = float(input("¿Cuántos pesos mexicanos tienes?: "))
         valor_dolar = 20.47
         dolares = pesos_mexicanos / valor_dolar
         dolares = round(dolares, 2)
         dolares = str(dolares)
         print("Tienes $" + dolares + " dólares")
-    else:
+    elif opcion == 2:
         dolares = float(input("¿Cuántos dolares tienes?: "))
         valor_mxn = 0.049
         mxn = dolares / valor_mxn
         mxn = round(mxn, 2)
         mxn = str(mxn)
         print("Tienes " + mxn + " pesos mexicanos")
+    elif opcion == 3:
+        pesos_colombianos = float(input("¿Cuántos pesos colombianos tienes?: "))
+        valor_dolar = 3991.33
+        dolares = pesos_colombianos/valor_dolar
+        dolares = round(dolares, 2)
+        dolares = str(dolares)
+        print("Tienes $" + dolares + " dólares")
+    else:
+        pesos_argentinos = float(input("¿Cuantos pesos argentinos tienes?: "))
+        valor_dolar = 122.31
+        dolares = pesos_argentinos/valor_dolar
+        dolares = round(dolares,2)
+        dolares = str(dolares)
+        print("Tienes $" + dolares + " dólares")
+    
 else:
     print("Opción inexistente")
